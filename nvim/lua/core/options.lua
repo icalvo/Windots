8c64d2f6343eb798a92b190e8f29384e47dd8d07
Ignacio Calvo <ignacio.calvo@gmail.com>
2025-09-09 11:07:28 +0200

    nvim: removed blinking cursor

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -48,8 +48,8 @@ opt.breakindentopt = "shift:2"
 opt.showbreak = "↳"
 
 -- Make cursor blink
-opt.guicursor = {
-    "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50",
-    "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
-    "sm:block-blinkwait175-blinkoff150-blinkon175",
-}
+-- opt.guicursor = {
+--     "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50",
+--     "a:blinkwait700-blinkoff1400-blinkon1250-Cursor/lCursor",
+--     "sm:block-blinkwait175-blinkoff150-blinkon175",
+-- }
