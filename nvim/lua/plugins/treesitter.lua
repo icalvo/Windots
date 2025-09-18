07dddde5ae14947ea0dc18666b64d196b404d4a4
Ignacio Calvo <ignacio.calvo@gmail.com>
2025-09-18 09:58:05 +0200

    Treesitter incr selection!

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -9,6 +9,13 @@ return {
     opts = {
         highlight = { enable = true },
         indent = { enable = true },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                node_incremental = "v",
+                node_decremental = "V",
+            },
+        },
         textobjects = {
             select = {
                 enable = true,
