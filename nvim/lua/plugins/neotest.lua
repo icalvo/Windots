568df9a75b11f615fff93456cb6760c61ace2217
Ignacio Calvo <ignacio.calvo@gmail.com>
2025-09-21 00:19:24 +0200

    Renamed some plugin configs

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -10,5 +10,15 @@ return {
         "nvim-lua/plenary.nvim",
         "antoinemadec/FixCursorHold.nvim",
         "nvim-treesitter/nvim-treesitter",
+        "marilari88/neotest-vitest",
+        "nvim-neotest/neotest-jest",
     },
+    config = function()
+        require("neotest").setup({
+            adapters = {
+                require("neotest-vitest"),
+                require("neotest-jest"),
+            },
+        })
+    end,
 }
