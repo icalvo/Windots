5cc89411ce20e38f2f7df8ef7435512e04aa21ad
Ignacio Calvo <ignacio.calvo@gmail.com>
2025-09-22 18:44:50 +0200

    Big keymaps refactor
    Keymaps reorganization. Also brought which-key headers to this file.

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -53,6 +53,9 @@ local function open_mini_files_safe()
 end
 
 -- stylua: ignore start
+local function wk_add_group(prefix, desc)
+    require("which-key").add({ mode = { "n", "v" }, { prefix, group = desc } })
+end
 
 -- better up/down
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true })
@@ -73,6 +76,18 @@ map("n", "L", "$", { desc = "Go to end of line" })
 map("o", "H", "_", { desc = "Move to start of line after ws" })
 map("o", "L", "$", { desc = "Move to end of line" })
 map("n", "<C-a>", "ggVG", { desc = "Select all the buffer" })
+
+-- Clear search with <esc>
+map("n", "<esc>", ":noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+
 
 -- Move to window using the <ctrl> hjkl keys
 map("n", "<A-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
@@ -99,50 +114,34 @@ map("v", "<C-A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { des
 map("v", "<C-A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
 -- Buffers
+wk_add_group("<leader>b", "buffer")
 map("n", "<leader>bb", function() utils.switch_to_other_buffer() end, { desc = "Switch to Other Buffer" })
 map("n", "<leader>bd", function() snacks.bufdelete({ wipe = true }) end, { desc = "Delete buffer" })
 
--- lazy
-map("n", "<leader>l", ":Lazy<cr>", { desc = "Lazy" })
+-- Package manager
+map("n", "<leader>l", ":Lazy<cr>", { desc = "Package manager" })
 
--- mini.files
+-- File explorer
 map("n", "<leader>x", open_mini_files_safe, { desc = "Open mini.files (cwd)" })-- Snacks Picker
 
 -- pickers
+wk_add_group("<leader>f", "file/find")
 map("n", "<leader><leader>", function() snacks.picker.smart() end, { desc = "Smart Fuzzy Find" })
+map("n", "<leader>fb", function() snacks.picker.buffers({ layout = { preset = "select" }}) end, { desc = "Fuzzy find buffers" })
+map("n", "<leader>fc", function() snacks.picker.grep_word() end, { desc = "Find word under cursor in CWD" })
+map("n", "<leader>fd", function() snacks.picker.todo_comments() end, { desc = "Todo" })
 map("n", "<leader>ff", function() snacks.picker.files({ hidden = true }) end, { desc = "Fuzzy find files" })
+map("n", "<leader>fh", function() snacks.picker.help() end, { desc = "Find help tags" })
 map("n", "<leader>fr", function() snacks.picker.recent() end, { desc = "Fuzzy find recent files" })
 map("n", "<leader>fs", function() snacks.picker.grep() end, { desc = "Find string in CWD" })
-map("n", "<leader>fc", function() snacks.picker.grep_word() end, { desc = "Find word under cursor in CWD" })
-map("n", "<leader>fb", function() snacks.picker.buffers({ layout = { preset = "select" }}) end, { desc = "Fuzzy find buffers" })
-map("n", "<leader>ft", function() snacks.picker() end, { desc = "Other pickers..." })
-map("n", "<leader>fh", function() snacks.picker.help() end, { desc = "Find help tags" })
 map("n", "<leader>fS", function() require("pick-resession").pick() end, { desc = "Find Session" })
-map("n", "<leader>fd", function() snacks.picker.todo_comments() end, { desc = "Todo" })
+map("n", "<leader>ft", function() snacks.picker() end, { desc = "Other pickers..." })
 
 -- flash
 map({ "n", "x", "o" }, "s", function() require("flash").jump() end,  { desc = "Flash" })
 map({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
 map({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
 map({ "c" }, "<c-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
-
--- toggle options
-utils.toggle_global_boolean("autoformat", "Autoformat"):map("<leader>ta")
-snacks.toggle(copilot_toggle_opts):map("<leader>tc")
-snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>ts")
-snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>tw")
-snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>tL")
-snacks.toggle.diagnostics():map("<leader>td")
-snacks.toggle.line_number():map("<leader>tl")
-snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>tC")
-snacks.toggle.treesitter():map("<leader>tT")
-if vim.lsp.inlay_hint then snacks.toggle.inlay_hints():map("<leader>th") end
-
--- browse to git repo
-map("n", "<leader>gb", function() snacks.gitbrowse() end, { desc = "Git Browse" })
-
--- Clear search with <esc>
-map("n", "<esc>", ":noh<cr><esc>", { desc = "Escape and clear hlsearch" })
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 map("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
@@ -152,10 +151,35 @@ map("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result
 map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 
+-- toggle options
+local function map_toggle(keymap, toggle)
+    toggle:map(keymap)
+end
+wk_add_group("<leader>s", "switch")
+map_toggle("<leader>sa", utils.toggle_global_boolean("autoformat", "Autoformat"))
+map_toggle("<leader>sc", snacks.toggle(copilot_toggle_opts))
+map_toggle("<leader>sC", snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }))
+map_toggle("<leader>sd", snacks.toggle.diagnostics())
+if vim.lsp.inlay_hint then
+    map_toggle("<leader>sh", snacks.toggle.inlay_hints())
+end
+map_toggle("<leader>sl", snacks.toggle.line_number())
+map_toggle("<leader>sL", snacks.toggle.option("relativenumber", { name = "Relative Number" }))
+map_toggle("<leader>ss", snacks.toggle.option("spell", { name = "Spelling" }))
+map_toggle("<leader>sT", snacks.toggle.treesitter())
+map_toggle("<leader>sw", snacks.toggle.option("wrap", { name = "Wrap" }))
+
+-- VCS
+wk_add_group("<leader>g", "git")
+map("n", "<leader>gb", function() snacks.gitbrowse() end, { desc = "Git Browse" })
+map("n", "<leader>gg", function() snacks.lazygit() end, { desc = "Lazygit" })
+map("n", "<esc>", ":noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+
 -- quit
 map("n", "<leader>qq", ":qa<cr>", { desc = "Quit all" })
 
 -- windows
+wk_add_group("<leader>w", "windows")
 map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
 map("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
 map("n", "<leader>w-", "<C-W>s", { desc = "Split window below", remap = true })
@@ -165,36 +189,56 @@ map("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
 map("n", "<leader>vz", function() snacks.zen() end, { desc = "Zen mode" })
 
 -- tabs
+wk_add_group("<leader><Tab>", "tabs")
 map("n", "<leader><tab><tab>", ":tabnew<cr>", { desc = "New Tab" })
 map("n", "<leader><tab>l", ":tabnext<cr>", { desc = "Next Tab" })
 map("n", "<leader><tab>d", ":tabclose<cr>", { desc = "Close Tab" })
 map("n", "<leader><tab>h", ":tabprevious<cr>", { desc = "Previous Tab" })
 
 -- Code/LSP
+wk_add_group("<leader>c", "code")
 map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
-map("n", "<leader>cd", function() vim.diagnostic.open_float({border = 'rounded'}) end, { desc = "Line Diagnostics" })
+map("n", "<leader>c?", function() vim.diagnostic.open_float({border = 'rounded'}) end, { desc = "Line Diagnostics" })
+map("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
+map("n", "<leader>cd", function() snacks.picker.lsp_definitions() end, { desc = "Goto Definitions" })
+map("n", "<leader>ch", function() return vim.lsp.buf.hover() end, { desc = "Hover" })
+map("n", "<leader>cJ", function() snacks.picker.lsp_implementations() end, { desc = "Goto Implementations" })
 map("n", "<leader>cl", ":check lsp<cr>", { desc = "LSP Info" })
+map("n", "<leader>cs", vim.lsp.buf.signature_help, { desc = "Signature Help" })
+map("n", "<leader>ct", function() snacks.picker.lsp_type_definitions() end, { desc = "Goto Type Definitions" })
+map("n", "<leader>cu", function() snacks.picker.lsp_references() end, { desc = "Goto Usages" })
+map("n", "<leader>cU", "[fgsu", { desc = "Goto Usages of containing method", remap = true })
+
+-- Debugging
+wk_add_group("<leader>d", "debugging")
+
+-- Refactoring
+wk_add_group("<leader>r", "refactor")
 map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
-map("n", "K", function() return vim.lsp.buf.hover() end, { desc = "Hover" })
-map("n", "gsD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
-map("n", "gK", vim.lsp.buf.signature_help, { desc = "Signature Help" })
-map("n", "gsu", function() snacks.picker.lsp_references() end, { desc = "Goto Usages" })
-map("n", "gsU", "[fgsu", { desc = "Goto Usages of containing method", remap = true })
-map("n", "gsJ", function() snacks.picker.lsp_implementations() end, { desc = "Goto Implementations" })
-map("n", "gsd", function() snacks.picker.lsp_definitions() end, { desc = "Goto Definitions" })
-map("n", "gst", function() snacks.picker.lsp_type_definitions() end, { desc = "Goto Type Definitions" })
+map("x", "<leader>re", ":Refactor extract ", { desc = "Extract" })
+map("x", "<leader>rf", ":Refactor extract_to_file ", { desc = "Extract to file" })
+map("x", "<leader>rv", ":Refactor extract_var ", { desc = "Extract variable" })
+map({ "n", "x" }, "<leader>ri", ":Refactor inline_var", { desc = "Inline variable" })
+map("n", "<leader>rI", ":Refactor inline_func", { desc = "Inline function" })
+map("n", "<leader>rb", ":Refactor extract_block", { desc = "Extract block" })
+map("n", "<leader>rbf", ":Refactor extract_block_to_file", { desc = "Extract block to file" })
+
+-- REST Client
+wk_add_group("<leader>R", "REST")
 
 -- Terminal/Run...
+wk_add_group("<leader>e", "execute")
 map({"n", "t"}, "<C-\\>", function() snacks.terminal() end, { desc = "Toggle Terminal" })
-map("n", "<leader>gg", function() snacks.lazygit() end, { desc = "Lazygit" })
-map("n", "<leader>kk", function() utils.open_terminal_toggle({ "k9s" }, true) end, { desc = "K9s" })
-map("n", "<leader>ao", function() utils.open_terminal_toggle({ "opencode", "." }) end, { desc = "Opencode" })
-map("n", "<leader>rlf", ":luafile %<cr>", { desc = "Run Current Lua File" })
-map("n", "<leader>rlt", ":PlenaryBustedFile %<cr>", { desc = "Run Lua Test File" })
-map("n", "<leader>rss", run_non_interactive_cmd(vim.fn.expand("%:p")), { desc = "Run shell script" })
-map("n", "<leader>rm", run_non_interactive_cmd("make"), { desc = "Run make" })
-map("n", "<leader>rt", run_non_interactive_cmd("task"), { desc = "Run task" })
--- test
-map("n", "<leader>rrt", ":Neotest run<cr>", { desc = "Run tests" })
+map("n", "<leader>ek", function() utils.open_terminal_toggle({ "k9s" }, true) end, { desc = "K9s" })
+wk_add_group("<leader>el", "lua")
+map("n", "<leader>elf", ":luafile %<cr>", { desc = "Run Current Lua File" })
+map("n", "<leader>elt", ":PlenaryBustedFile %<cr>", { desc = "Run Lua Test File" })
+wk_add_group("<leader>es", "shell")
+map("n", "<leader>ess", run_non_interactive_cmd(vim.fn.expand("%:p")), { desc = "Run shell script" })
+map("n", "<leader>em", run_non_interactive_cmd("make"), { desc = "Run make" })
+map("n", "<leader>et", run_non_interactive_cmd("task"), { desc = "Run task" })
 
+-- test
+wk_add_group("<leader>t", "test")
+map("n", "<leader>tt", ":Neotest run<cr>", { desc = "Run tests" })
 -- stylua: ignore end
