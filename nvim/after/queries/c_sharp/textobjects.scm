251f354f71c99f88a72238f5c80cb4187a9739e9
Ignacio Calvo <ignacio.calvo@gmail.com>
2025-09-22 12:36:19 +0200

    Candidate for function detector

--- nvim/after/queries/c_sharp/textobjects.scm
+++ nvim/after/queries/c_sharp/textobjects.scm
@@ -4,16 +4,19 @@
 
 (class_declaration
   name: (_) @class.name)
-; better function.* that supports arrow functions
-(method_declaration
-  body: [
-    (arrow_expression_clause (_) @function.inner) @function.outer
-     (block
-       .
-       "{"
-       .
-       (_) @_start @_end
-       (_)? @_end
-       .
-       "}"
-       (#make-range! "function.inner" @_start @_end)) @function.outer])
+;; better function.* that supports arrow functions
+;(method_declaration
+;  body: [
+;    (arrow_expression_clause (_) @function.inner) @function.outer
+;    (block
+;      .
+;      "{"
+;      .
+;      (_) @_start @_end
+;      (_)? @_end
+;      .
+;      "}"
+;      (#make-range! "function.inner" @_start @_end)
+;    ) @function.outer
+;  ]
+;)
