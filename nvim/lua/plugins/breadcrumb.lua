e00cee28ceb696142574cc227a8b1e7d98ba77c8
Ignacio Calvo <ignacio.calvo@gmail.com>
2025-09-29 11:00:18 +0200

    Simplified navic setup

--- nvim/lua/plugins/breadcrumb.lua
+++ nvim/lua/plugins/breadcrumb.lua
@@ -1,19 +1,17 @@
 return {
     "smiteshp/nvim-navic",
     lazy = false,
-    config = function()
-        require("nvim-navic").setup({
-            lsp = {
-                auto_attach = true,
-                -- priority order for attaching LSP servers
-                -- to the current buffer
-                preference = {
-                    "html",
-                    "templ",
-                },
+    opts = {
+        lsp = {
+            auto_attach = true,
+            -- priority order for attaching LSP servers
+            -- to the current buffer
+            preference = {
+                "html",
+                "templ",
             },
-            highlight = true,
-            separator = " 󰁔 ",
-        })
-    end,
+        },
+        highlight = true,
+        separator = " 󰁔 ",
+    },
 }
