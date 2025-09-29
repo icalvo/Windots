24565d2b653980d8cbf0c6d915c67d1a53ef6ee2
Ignacio Calvo <ignacio.calvo@gmail.com>
2025-09-29 10:59:03 +0200

    Removed unused lsp stuff

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -9,24 +9,25 @@ vim.diagnostic.config({
 })
 vim.lsp.enable({
     "bicep",
-    "docker_compose_language_service",
+    -- "docker_compose_language_service",
     "html",
     "jsonls",
     "luals",
     "ols",
     "powershell_es",
-    "pyright",
+    -- "pyright",
     "rust_analyzer",
     "tailwindcss",
     "taplo",
     "templ",
-    "terraformls",
+    -- "terraformls",
     "ts_ls",
     "yamlls",
 })
-vim.lsp.config('docker_compose_language_service', {})
-vim.lsp.config('pyright', {})
-vim.lsp.config('kulala-ls', {
-    cmd = { 'kulala-ls', '--stdio' },
-    filetypes = { 'http' },
-    root_markers = { '.git' }})
+-- vim.lsp.config("docker_compose_language_service", {})
+-- vim.lsp.config("pyright", {})
+vim.lsp.config("kulala-ls", {
+    cmd = { "kulala-ls", "--stdio" },
+    filetypes = { "http" },
+    root_markers = { ".git" },
+})
