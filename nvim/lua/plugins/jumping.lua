e944e7a605b1a13ec537a79d9bc51e59b9811590
Ignacio Calvo <ignacio.calvo@gmail.com>
2025-09-29 11:00:52 +0200

    Removed flash char searches

--- nvim/lua/plugins/jumping.lua
+++ nvim/lua/plugins/jumping.lua
@@ -1,8 +1,15 @@
 return {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {
+            modes = {
+                char = {
+                    enabled = false,
+                },
+            },
+        },
       -- stylua: ignore
       keys = {
         { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
@@ -11,4 +18,5 @@ return {
         { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
         { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
       },
+    },
 }
