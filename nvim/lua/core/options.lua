372cee1d0b312feeed123f972b466cfd36086275
Ignacio Calvo <ignacio.calvo@gmail.com>
2025-10-11 23:47:27 +0200

    Removed perl and ruby providers

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 vim.g.user = os.getenv("USERNAME") or os.getenv("USER")
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 local opt = vim.opt
 opt.shell = "zsh"
