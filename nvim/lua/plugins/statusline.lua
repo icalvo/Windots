ec00e3099518c1bb2c58801aa16c7ecb3f0c4427
Ignacio Calvo <ignacio.calvo@gmail.com>
2025-10-16 14:08:19 +0200

    Added noice to statusline

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -52,7 +52,31 @@ return {
                 "diagnostics",
             },
             lualine_c = { "filename", { "navic", color_correction = "dynamic" } },
-            lualine_x = { { "macro_recording", "%S" }, "encoding", "fileformat", "filetype" },
+            lualine_x = {
+                {
+                    require("noice").api.status.message.get_hl,
+                    cond = require("noice").api.status.message.has,
+                },
+                {
+                    require("noice").api.status.command.get,
+                    cond = require("noice").api.status.command.has,
+                    color = { fg = "#ff9e64" },
+                },
+                {
+                    require("noice").api.status.mode.get,
+                    cond = require("noice").api.status.mode.has,
+                    color = { fg = "#ff9e64" },
+                },
+                {
+                    require("noice").api.status.search.get,
+                    cond = require("noice").api.status.search.has,
+                    color = { fg = "#ff9e64" },
+                },
+                { "macro_recording", "%S" },
+                "encoding",
+                "fileformat",
+                "filetype",
+            },
             lualine_y = { "progress" },
             lualine_z = { "location" },
         },
